235c8c311d1e086af094a058672edcf629e1ed9d
Kamuri Amorim <myk.gata14@gmail.com>
2025-11-26 19:14:07 -0300

    fix: a

--- static/script.js
+++ static/script.js
@@ -10,6 +10,7 @@ if (window.location.pathname.startsWith(`${window.CONTEXT_PATH}/dashboard`)) {
   fetchPokemonGenerations();
 }
 
+const originalFetch = window.fetch;
 const accessId = crypto.randomUUID();
 
 if (window.DD_RUM && window.DD_RUM.setGlobalContextProperty) {
@@ -19,6 +20,43 @@ if (window.DD_RUM && window.DD_RUM.setGlobalContextProperty) {
 window.login = login;
 window.register = register;
 window.buildPokemonList = buildPokemonList;
+
+(async () => {
+  window.fetch = async (...args) => {
+    let [resource, config] = args;
+
+    config = config || {};
+
+    const baggageKey = "baggage";
+    const baggageValue = "x-correlation-id=" + accessId;
+
+    if (!config.headers) {
+      config.headers = {};
+    }
+
+    let urlString = resource;
+    if (resource instanceof Request) {
+      urlString = resource.url;
+    }
+
+    const isInternalRequest =
+      urlString.toString().startsWith("/") ||
+      urlString.toString().includes("tools.kamuridesu.com") ||
+      urlString.toString().includes("localhost");
+
+    if (isInternalRequest) {
+      if (config.headers instanceof Headers) {
+        config.headers.set(baggageKey, baggageValue);
+      } else {
+        config.headers[baggageKey] = baggageValue;
+      }
+    }
+
+    const response = await originalFetch(resource, config);
+
+    return response;
+  };
+})();
 
 (async () => {
   const elem = document.createElement("a");
