3b0375594473062d8a907bab8d1812824d12a106
Kamuri Amorim <myk.gata14@gmail.com>
2025-11-26 12:04:45 -0300

    fix: middleware instrumentation

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 
 import hypercorn as hc
 import hypercorn.asyncio as hcaio
-from ddtrace import tracer
 from ddtrace.contrib.asgi import TraceMiddleware
 from quart import Blueprint, Quart
 
@@ -34,7 +33,7 @@ async def main():
         url_prefix=CONTEXT_PATH,
     )
 
-    app.asgi_app = TraceMiddleware(app.asgi_app, tracer=tracer, service="pokelogger")
+    app.asgi_app = TraceMiddleware(app.asgi_app)
 
     config = hc.Config()
     config.bind = "0.0.0.0:8080"
